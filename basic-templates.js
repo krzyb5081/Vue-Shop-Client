9f019af8aa4a81a92792978f2d31a0b036905317
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2021-02-23 22:12:17 +0100

    basic website with grid 'done'

---
+++ basic-templates.js
@@ -0,0 +1,43 @@
+Vue.component('header-template',{
+    template: `
+    <div class="header" style="background-color: #5a5af4; display: grid; grid-template: 100% / 20% auto 20%;">
+        <div>
+            <img style="height: 100%; width: 100%; object-fit: contain;" src="binco-logo.png"></img>
+        </div>
+        <div style="text-align: center; margin: 10px; padding: 10px">
+            <h1>Welcome to Binco clothing shop</h1>
+        </div>
+        <div style="text-align: right; margin: 10px; padding: 10px">
+            <button>Log in</button>
+        </div>
+    </div>
+    `
+})
+
+Vue.component('sidebar-template',{
+    template: `
+    <div class="sidebar" style="background-color: blueviolet; display: grid; grid-template-columns: auto auto;">
+        <div style="text-align: center;">
+            <h4>made by: krzyb5081</h4>
+        </div>
+        <div style="text-align: center;">
+            No rights reserved, blah blah, something.<br>
+            Have a good shopping.
+        </div>
+    </div>
+    `
+})
+
+Vue.component('footer-template',{
+    template: `
+    <div class="footer" style="background-color: #5a5af4; display: grid; grid-template-columns: auto auto;">
+        <div style="text-align: center;">
+            <h4>made by: krzyb5081</h4>
+        </div>
+        <div style="text-align: center;">
+            No rights reserved, blah blah, something.<br>
+            Have a good shopping.
+        </div>
+    </div>
+    `
+})

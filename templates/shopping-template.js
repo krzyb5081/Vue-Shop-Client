edd0d6d4c2f172193ea4ca9e6c4184ccac779c23
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2021-02-28 23:41:13 +0100

    starting to make /shopping template

--- templates/shopping-template.js
+++ templates/shopping-template.js
@@ -1,22 +1,58 @@
 Vue.component("shopping-template",{
     template:`
         <div>
-            <p>{{ data }}</p>
+            <form v-on:submit="search()" action="#/shopping" method="post">
+                <input type="text" v-model="searchText" name="searchText" placeholder="product name" />
+                <input type="submit" value="Search" />
+            </form>
+
+            <table>
+                <tr>
+                    <th>Product name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Available</th>
+                    <th>Buy</th>
+                </tr>
+                
+                <tr v-for="product in products">
+                    <td> {{product.name}} </td>
+                    <td> {{product.description}} </td>
+                    <td> {{product.price}} </td>
+                    <td> {{product.quantityAvailable}} </td>
+                    
+                    <td>
+                        <form v-on:submit="addProductToCart(product.id)" action="#" method="post">
+                            <input type="number" v-model="quantity" name="quantity" value="0" />
+                            <input type="submit" value="Add to cart" />
+                        </form>
+                    </td>
+                </tr>
+            </table>
         </div>
     `,
-    name: "shopping-t",
     data: function(){
         return {
-            data: {}
+            products: {},
+            searchText: null,
+            quantity: null
         }
     },
     methods:{
-        getData: async function(){
+        getProducts: async function(){
             let response = await axios.get('http://localhost:8080/shopping')
-            .then(resp => {this.data = resp.data});
+            .then(resp => {this.products = resp});
+        },
+        search: function(){
+
+        },
+        addProductToCart: function(productId){
+            alert("product id: "+productId+" quantity: "+this.quantity)
         }
     },
     mounted(){
-        this.getData();
+        this.getProducts();
     }
 })
+//<form action="#" th:action="@{/shopping}" method="post">
+//<form action="#" th:action="@{/addProductToCart}" method="post">

ab8a32305cc5fbd3c2299fd59cecb9f9855e2a6b
Krzysztof <krzysztof.sobczak5081@gmail.com>
2024-05-02 09:37:55 +0200

    Update HeaderTemplate.vue
    updated via browser

--- src/templates/HeaderTemplate.vue
+++ src/templates/HeaderTemplate.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="header" style="display: grid; grid-template: 100% / 20% auto 20%; background-color: blue;">
+    <div class="header">
         <div>
             <router-link to="/">
-                <img style="height: 100%; width: 100%; object-fit: contain;" src="./img/binco-logo.png"/>
+                <img id="logo" src="./img/binco-logo.png"/>
             </router-link>
         </div>
         <div style="text-align: center; margin: 10px; padding: 10px">
             <h3>Welcome to Binco clothing shop</h3>
         </div>
-        <div style="text-align: right; margin: 10px; padding: 10px; display: grid; grid-row: 30% auto;">
+        <div id="header-buttons">
             <div v-if="this.$store.state.logged === false">
                 <router-link to="/login"><button class="btn btn-secondary btn-sm btn-block">Log in</button></router-link>
                 <router-link to="/register"><button class="btn btn-secondary btn-sm btn-block">Register</button></router-link>

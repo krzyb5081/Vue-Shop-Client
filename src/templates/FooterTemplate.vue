36f112971aa10ee9b590908a6d4f191867c9b814
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-05-07 21:01:27 +0200

    Repair css
    Restore back to normal so i know what does what

--- src/templates/FooterTemplate.vue
+++ src/templates/FooterTemplate.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="footer">
-        <div style="text-align: center; background-color: blueviolet;">
+        <div>
             Front-End and Back-End made by: krzyb5081<br>
             <a href="https://github.com/krzyb5081/" target="_blank">
                 <button class="btn btn-secondary">Github: krzyb5081</button>
             </a>
         </div>
-        <div style="text-align: center;">
+        <div>
             Some text here.<br>
             Have a good shopping.
         </div>

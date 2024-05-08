f3efb57cd6dd16d1daef7dbfd6f61d0a812656e6
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-05-08 20:08:32 +0200

    Site work on Bootstrap
    Preparing to use bootstrap instead of css, im still learning :P

--- src/templates/FooterTemplate.vue
+++ src/templates/FooterTemplate.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="footer">
-        <div>
-            Front-End and Back-End made by: krzyb5081<br>
-            <a href="https://github.com/krzyb5081/" target="_blank">
-                <button class="btn btn-secondary">Github: krzyb5081</button>
-            </a>
-        </div>
-        <div>
-            Some text here.<br>
-            Have a good shopping.
+    <div class="jumbotron text-center bg-dark">
+        <div class="row justify-content-around">
+            <div class="col-sm-4">
+                Front-End and Back-End made by: krzyb5081<br>
+                <a href="https://github.com/krzyb5081/" target="_blank">
+                    <button class="btn btn-secondary">Github: krzyb5081</button>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                Some text here.<br>
+                Have a good shopping.
+            </div>
         </div>
     </div>
 </template>
--- src/templates/HeaderTemplate.vue
+++ src/templates/HeaderTemplate.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="header">
-        <div>
-            <router-link to="/">
-                <img id="header-logo" src="./img/binco-logo.png"/>
-            </router-link>
-        </div>
-        <div class="header-welcome">
-            <h3>Welcome to Binco clothing shop</h3>
-        </div>
-        <div id="header-buttons">
-            <div v-if="this.$store.state.logged === false">
-                <router-link to="/login"><button class="btn btn-secondary btn-sm btn-block">Log in</button></router-link>
-                <router-link to="/register"><button class="btn btn-secondary btn-sm btn-block">Register</button></router-link>
+    <div class="jumbotron text-center bg-dark">
+        <div class="row d-flex justify-content-between">
+            <div class="p-1 col-sm-4">
+                <router-link to="/">
+                    <img class="img-fluid" src="./img/binco-logo.png"/>
+                </router-link>
             </div>
-            <b v-else>Hello, {{this.$store.state.userName}}<br></b>
-            <router-link to="/showShoppingCart"><button class="btn btn-primary btn-lg btn-block">Show cart</button></router-link><br/>
+            <div class="p-1 col-sm-4">
+                <h3>Welcome to Binco clothing shop</h3>
+            </div>
+            <div class="p-1 col-sm-2">
+                <div v-if="this.$store.state.logged === false">
+                    <router-link to="/login"><button class="btn btn-secondary btn-sm btn-block">Log in</button></router-link>
+                    <router-link to="/register"><button class="btn btn-secondary btn-sm btn-block">Register</button></router-link>
+                </div>
+                <b v-else>Hello, {{this.$store.state.userName}}<br></b>
+                <router-link to="/showShoppingCart"><button class="btn btn-primary btn-lg btn-block">Show cart</button></router-link><br/>
+            </div>
         </div>
     </div>
 </template>

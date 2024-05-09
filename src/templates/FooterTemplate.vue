098caef2a4a12604de84f04e61a88628a02899e3
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-05-09 22:38:07 +0200

    New footer
    New footer based on Bootstrap

--- src/templates/FooterTemplate.vue
+++ src/templates/FooterTemplate.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="jumbotron text-center bg-dark">
-        <div class="row justify-content-around">
-            <div class="col-sm-4">
-                Front-End and Back-End made by: krzyb5081<br>
-                <a href="https://github.com/krzyb5081/" target="_blank">
-                    <button class="btn btn-secondary">Github: krzyb5081</button>
-                </a>
-            </div>
-            <div class="col-sm-4">
-                Some text here.<br>
-                Have a good shopping.
-            </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <span class="mb-3 mb-md-0 text-body-secondary">Front-End and Back-End made by: krzyb5081</span>
         </div>
-    </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3">
+            <a class="text-body-secondary" href="https://github.com/krzyb5081/" target="_blank">
+                <img class="bi" width="50" height="50" src="./svg/github-mark.svg">
+            </a>
+        </li>
+        </ul>
+    </footer>
 </template>

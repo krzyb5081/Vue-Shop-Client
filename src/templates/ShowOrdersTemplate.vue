37eacd1b2922f6352ecac09735345a0e33758d73
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-05-13 23:05:58 +0200

    Show orders template works
    data from server is now displayed properly on website

--- src/templates/ShowOrdersTemplate.vue
+++ src/templates/ShowOrdersTemplate.vue
@@ -21,11 +21,13 @@ export default defineComponent({
 
 <template>
     <div>
-        <h3>Feature under construction</h3>
-        <table>
+        <h3>Every order list</h3>
+        <table class="table table-dark">
             <tr>
-                <th>Id</th>
-                <th>Status</th>
+                <th scope="col">Id</th>
+                <th scope="col">Status</th>
+                <th scope="col">Customer</th>
+                <th scope="col">Ordered stuff</th>
             </tr>
             
             <tr v-for="order in orderList" v-bind:key="order.id">
@@ -36,42 +38,169 @@ export default defineComponent({
                 <td>
                     <span>{{ order.status }}</span>
                 </td>
+                <td>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">user name</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>{{ order.user.id }}</span>
+                        </td>
+                        <td>
+                            <span>{{ order.user.userName }}</span>
+                        </td>
+                    </tr>
+                </td>
+                <td>
+                    <tr>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                    <tr v-for="orderPart in order.orderPartList" v-bind:key="orderPart.id">
+                        <td>{{ orderPart.quantity }}</td>
+                        <td>{{ orderPart.product.name }}</td>
+                        <td>{{ orderPart.product.description }}</td>
+                        <td>{{ orderPart.product.price }}</td>
+                    </tr>
+                </td>
             </tr>
         </table>
     </div>
 </template>
 
-<!-- example order from orderList
-    Order:
-        {
-        "id":28,
-        "status":"paid",
-        "orderProductList":
-        [
+<!-- example orderList
+[
+    {
+        "id": 4,
+        "status": "not paid",
+        "user": {
+            "id": 1,
+            "userName": "user22",
+            "password": "user22password",
+            "type": "user",
+            "money": 50.0
+        },
+        "orderPartList": [
             {
-                "id":29,
-                "quantity":1,
-                "product":
-                {
-                    "id":2,
-                    "name":"komoda",
-                    "description":"drewniana komoda",
-                    "price":25.99,
-                    "quantityAvailable":26
+                "id": 5,
+                "quantity": 1,
+                "product": {
+                    "id": 3,
+                    "name": "Blue paint",
+                    "description": "blue",
+                    "price": 2.6,
+                    "quantityAvailable": 51
+                }
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "status": "not paid",
+        "user": {
+            "id": 6,
+            "userName": "user1",
+            "password": "user1",
+            "type": "user",
+            "money": 50.0
+        },
+        "orderPartList": [
+            {
+                "id": 8,
+                "quantity": 2,
+                "product": {
+                    "id": 2,
+                    "name": "Red paint",
+                    "description": "red",
+                    "price": 2.55,
+                    "quantityAvailable": 28
+                }
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "status": "not paid",
+        "user": {
+            "id": 6,
+            "userName": "user1",
+            "password": "user1",
+            "type": "user",
+            "money": 50.0
+        },
+        "orderPartList": [
+            {
+                "id": 10,
+                "quantity": 3,
+                "product": {
+                    "id": 2,
+                    "name": "Red paint",
+                    "description": "red",
+                    "price": 2.55,
+                    "quantityAvailable": 28
                 }
             },
             {
-                "id":30,
-                "quantity":1,
-                "product":
-                {
-                    "id":3,
-                    "name":"diesel efecta",
-                    "description":"drogie paliwo",
-                    "price":2.43,
-                    "quantityAvailable":55236
+                "id": 11,
+                "quantity": 5,
+                "product": {
+                    "id": 3,
+                    "name": "Blue paint",
+                    "description": "blue",
+                    "price": 2.6,
+                    "quantityAvailable": 51
+                }
+            }
+        ]
+    },
+    {
+        "id": 13,
+        "status": "not paid",
+        "user": {
+            "id": 6,
+            "userName": "user1",
+            "password": "user1",
+            "type": "user",
+            "money": 50.0
+        },
+        "orderPartList": [
+            {
+                "id": 14,
+                "quantity": 500,
+                "product": {
+                    "id": 12,
+                    "name": "grzyb",
+                    "description": "różowy z czarnymi kropkami ",
+                    "price": 0.04,
+                    "quantityAvailable": 99500
+                }
+            },
+            {
+                "id": 15,
+                "quantity": 17,
+                "product": {
+                    "id": 2,
+                    "name": "Red paint",
+                    "description": "red",
+                    "price": 2.55,
+                    "quantityAvailable": 28
+                }
+            },
+            {
+                "id": 16,
+                "quantity": 13,
+                "product": {
+                    "id": 3,
+                    "name": "Blue paint",
+                    "description": "blue",
+                    "price": 2.6,
+                    "quantityAvailable": 51
                 }
             }
         ]
     }
+]
 -->
